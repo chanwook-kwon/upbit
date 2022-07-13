ff19c3120a763800a19c55f3a05679e84b90195c
chanwook-kwon <109012474+chanwook-kwon@users.noreply.github.com>
2022-07-13 17:00:52 +0900

    Add files via upload

--- makemoney.py
+++ makemoney.py
@@ -13,18 +13,19 @@ upbit = pyupbit.Upbit(access, secret)
 #매수목표금액 = 현재가격의 -0.4%
 #매수목표금액 = 매수금액의 +0.5%
 
-Coin_ADA = 'KRW-ADA'
+Coin_ADA = 'KRW-ELF'
 ADA_Current_Price = pyupbit.get_current_price(Coin_ADA) #현재 코인 가격.
 ADA_OneStep_Price = 1
-ADA_OneStep_Percent = 0.16
-ADA_Buy_Step = 3 #3*0.16% = 0.48% 현재 가격의 0.48%에 매수함.
-ADA_Sell_Step = 4 #4*0.16 = 0.54% - 현재 가격의 0.54%에 매도함.
+ADA_OneStep_Percent = 0.39
+ADA_Buy_Step = 2 #2*0.39% = 0.78% 현재 가격의 0.78%에 매수함.
+ADA_Sell_Step = 4 #4*0.39 = 0.54% - 현재 가격의 0.54%에 매도함.
 ADA_Buy_Money = 5500 # 1회 매수금액
 ADA_Buy_Price = ADA_Current_Price-(ADA_Buy_Step*ADA_OneStep_Price)
 ADA_Buy_Quantity = ADA_Buy_Money / ADA_Buy_Price
 ADA_Try_Count = 0
 ADA_Buy_Count = 0
 ADA_Sell_Count = 0
+ADA_GAP = 0.5
 
 print("current price=",ADA_Current_Price)
 
@@ -39,11 +40,11 @@ while True:
         ADA_Ma60 = pyupbit.get_ohlcv(Coin_ADA, interval = 'minute5', count=60)
         ADA_Average_Ma30 = np.mean(ADA_Ma30['close'])
         ADA_Average_Ma60 = np.mean(ADA_Ma60['close'])
-        if (ADA_Balance > 0) and (abs(ADA_Average_Ma60 - ADA_Average_Ma30) >= 0.5) : #보유코인이 있으면 매도한다.(이익일수도 있고, 손절일수도 있고)
+        if (ADA_Balance > 0) and ((ADA_Average_Ma30 + ADA_GAP == ADA_Average_Ma60)) : #보유코인이 있으면 매도한다.(이익일수도 있고, 손절일수도 있고)
             ADA_ret_Sell = upbit.sell_market_order(Coin_ADA,ADA_Balance)
             print("매도성공",ADA_ret_Sell)
             ADA_Sell_Count = ADA_Sell_Count + 1
-        if (ADA_Balance == 0) and (abs(ADA_Average_Ma30 - ADA_Average_Ma60) >= 0.5) : #보유코인이 없으면 -> 매수 진행.
+        if (ADA_Balance == 0) and ((ADA_Average_Ma30 == ADA_Average_Ma60 + ADA_GAP)) : #보유코인이 없으면 -> 매수 진행.
             ADA_ret_buy = upbit.buy_market_order(Coin_ADA,ADA_Buy_Money)
             print("매수성공",ADA_ret_buy)
             ADA_Buy_Count = ADA_Buy_Count + 1
@@ -52,13 +53,17 @@ while True:
             print("ADA_Balance=",ADA_Balance)
             print("ADA_Average_Ma30=",ADA_Average_Ma30)
             print("ADA_Average_Ma60=",ADA_Average_Ma60)
-            print("매수조건 Ma30-Ma60=",abs(ADA_Average_Ma30-ADA_Average_Ma60))
-            print("매도조건 Ma30-Ma60=",abs(ADA_Average_Ma60-ADA_Average_Ma30))
+            if ADA_Average_Ma30 > ADA_Average_Ma60 :
+                print("매수가능 GAP=",(ADA_Average_Ma60-ADA_Average_Ma30))
+            else :
+                print("매도가능 GAP=",(ADA_Average_Ma30-ADA_Average_Ma60))
             ADA_Try_Count = ADA_Try_Count + 1
             print("ADA_Try_Count=",ADA_Try_Count)
             print("ADA_Buy_Count=",ADA_Buy_Count)
             print("ADA_Sell_Count=",ADA_Sell_Count)
             print("----------------------------")
+            if ADA_Buy_Count > 10 :
+                break
             time.sleep(10)
     except Exception as e:
             print(e)
